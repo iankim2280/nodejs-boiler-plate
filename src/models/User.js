dd97b5d86c2186b3bbc1a211f57d060d80fcc2ff
IanKim <iankim2280@gmail.com>
2020-09-19 01:29:19 -0400

    login and password bcrypt applied

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,8 @@
 // const mongoose = require("mongoose");
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const saltRounder = 10;
 
 const userSchema = mongoose.Schema({
   firstname: {
@@ -30,6 +33,50 @@ const userSchema = mongoose.Schema({
   token: String,
   tokenExp: Number,
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화
+    bcrypt.genSalt(saltRounder, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callBack) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callBack(err);
+    callBack(null, isMatch); // noerr, password is correct
+  });
+};
+
+userSchema.methods.generateToken = function (callBack) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cd(err);
+    callBack(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, callBack) {
+  var user = this;
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return callBack(err);
+      callBack(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
