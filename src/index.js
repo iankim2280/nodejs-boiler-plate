3bf7d65feaca968e404cbb05714274967300d7f9
IanKim <iankim2280@gmail.com>
2020-09-28 12:49:03 -0400

    NodeJs, ExpressJs, MongoDB;boiler-plate

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ import "./middlewares/db";
 dotenv.config();
 const app = express();
 const { PORT } = process.env;
-
 // when you open the localhost on browser
 app.get("/", (req, res) => res.send("hello world"));
+
+app.get("/api/hello", (req, res) => {
+  res.send("Hello from server.");
+});
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post("/api/users/signup", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할 떄 필요한 정보들을 client를 가져오면 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
 
@@ -33,12 +36,12 @@ app.post("/api/users/signup", (req, res) => {
 });
 
 app.post("/api/users/login", (req, res) => {
-  // 1. 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  // 1. Find a user using this email
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다. ",
+        message: "There's no user using this email. ",
       });
     }
     // 2. 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인.
@@ -46,7 +49,7 @@ app.post("/api/users/login", (req, res) => {
       if (!isMatch)
         return res.json({
           loginSuccess: false,
-          message: "비밀번호가 틀렸습니다.",
+          message: "Wrong password.",
         });
       // 3. if passwords are correct, generate token
       user.generateToken((err, user) => {
